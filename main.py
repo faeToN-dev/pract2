473b336f535afc8f46bd0ae715300e9007bf41bf
f@eToN <faeton@fs.if.ua>
2024-12-04 21:29:31 +0200

    ~

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
-async def root(request: Request):
-    ip = request.client.host
-    ret = f"Your IP: {ip}"
-    return ret
+def root():
+    return FileResponse("index.html")
+
+
+@app.post("/postdata")
+def postdata(number1 = Form(), number2=Form()):
+    n1 = float(number1)
+    n2 = float(number2)
+
+    if act == 'summ':
+        result = n1 + n2
+    elif act == 'minus':
+        result = n1 - n2
+    elif act == 'mult':
+        result = n1 * n2
+    elif act == 'div':
+        result = n1 / n2
+    else:
+        result = 'Operation impossible'
+
+    return {"result": result}
